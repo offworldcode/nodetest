46b4f495aea0dd653d2f21f8da0b55b92d0d9402
shawn <shawn.desarkar@gmail.com>
2019-01-15 22:31:03 -0700

    First Commit

---
+++ app2.js
@@ -0,0 +1,18 @@
+//const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+app.use(bodyParser.urlencoded({ extended : false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
+
+app.listen(8080);
+//server.listen(8080);
